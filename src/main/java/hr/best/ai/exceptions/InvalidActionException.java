de628890bada65f2543fe3f17e370ecd27c3e544
Neven Miculinic <neven.miculinic@gmail.com>
2015-05-30 23:49:55 +0200

    Added constructor

--- src/main/java/hr/best/ai/exceptions/InvalidActionException.java
+++ src/main/java/hr/best/ai/exceptions/InvalidActionException.java
@@ -4,4 +4,7 @@ package hr.best.ai.exceptions;
  * Created by lpp on 5/3/15.
  */
 public class InvalidActionException extends Exception {
+    public InvalidActionException(Exception ex) {
+        super(ex);
+    }
 }
