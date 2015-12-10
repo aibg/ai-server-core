806febf2cc523bd5d8f7cacd3c95d02183d9c3a1
andrej <andrej@161.53.77.188>
2015-12-10 11:07:51 +0100

    AbstractPlayer documented

--- src/main/java/hr/best/ai/gl/AbstractPlayer.java
+++ src/main/java/hr/best/ai/gl/AbstractPlayer.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 
 import com.google.gson.JsonObject;
 
+/**
+ * Most generalized player class.
+ */
 public abstract class AbstractPlayer implements AutoCloseable {
 
     private final String name;
@@ -13,11 +16,26 @@ public abstract class AbstractPlayer implements AutoCloseable {
     public AbstractPlayer(String name) {
         this.name = name;
     }
-
+    /**
+     * Sends error message to player 
+     * @param message the message
+     */
     public abstract void sendError(JsonObject message);
 
+    /**
+     * Retrieves player's action.
+     * 
+     * @param state
+     *            JsonObject sent to player
+     * @return JsonObject containing player action
+     * @throws IOException in case of communication error
+     * @throws InvalidActionException in case JsonObject can't be created from player's output
+     */
     public abstract JsonObject signalNewState(JsonObject state) throws IOException, InvalidActionException;
 
+    /**
+     * @return player name
+     */
     public String getName() {
         return this.name;
     }
