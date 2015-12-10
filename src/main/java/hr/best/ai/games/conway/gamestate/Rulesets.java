8c9477bfc6a092435d90d206cf4d0aa07b441318
andrej <andrej@161.53.77.188>
2015-12-10 11:01:56 +0100

    Rulesets documented

--- src/main/java/hr/best/ai/games/conway/gamestate/Rulesets.java
+++ src/main/java/hr/best/ai/games/conway/gamestate/Rulesets.java
@@ -7,11 +7,22 @@ import java.util.HashMap;
 import java.util.function.Function;
 
 /**
- * Created by lpp on 11/1/15.
+ * Collection of static classes representing different rules for determining how
+ * next state gets calculated. It's a singleton.
  */
 public class Rulesets {
-
+	/**
+	 * Classic ruleset:<br>
+	 * same four rules as found on wikipedia's "Conway's Game of Life" article
+	 * with the difference: dead cell doesn't activate in case of enemy active
+	 * neighbors
+	 */
     public static class Ruleset1 {
+    	
+    	/**
+    	 * Cell activates only with exactly 3 friendly neighbors and 0 enemy
+    	 * neighbors
+    	 */
         public final static Integer fromEmpty(Pair<Integer, Integer> a) {
             if (a.getLeft() == 3 && a.getRight() == 0)
                 return ConwayGameStateConstants.PLAYER1_CELL;
@@ -19,14 +30,27 @@ public class Rulesets {
                 return ConwayGameStateConstants.PLAYER2_CELL;
             return ConwayGameStateConstants.DEAD_CELL;
         }
-
+        
+        /**
+         * Cell stays active only with 2 or 3 friendly neighbors
+         */
         public final static Integer fromOccupied(Triple<Integer, Integer, Integer> a) {
             return a.getLeft() == 2 || a.getLeft() == 3
                     ? a.getRight() : ConwayGameStateConstants.DEAD_CELL;
         }
     }
 
+    /**
+     * Diff ruleset:<br>
+     * same four rules as found on wikipedia's "Conway's Game of Life" article
+     * with the difference being that neighbors are calculated by subtracting
+     * enemy neighbors from friendly neighbors
+     */
     public static class DiffRuleset {
+    	
+    	/**
+    	 * Cell activates only with 2 or 3 neighbors (#friendly neighbors-#enemy neighbors)
+    	 */
         public final static Integer fromEmpty(Pair<Integer, Integer> a) {
             if (a.getLeft() - a.getRight() == 3)
                 return ConwayGameStateConstants.PLAYER1_CELL;
@@ -34,7 +58,10 @@ public class Rulesets {
                 return ConwayGameStateConstants.PLAYER2_CELL;
             return ConwayGameStateConstants.DEAD_CELL;
         }
-
+        
+        /**
+         * Cell activates only with 2 or 3 neighbors (friendly-enemy)
+         */
         public final static Integer fromOccupied(Triple<Integer, Integer, Integer> a) {
             switch (a.getLeft() - a.getMiddle()) {
                 case 2:
@@ -46,12 +73,22 @@ public class Rulesets {
         }
     }
 
+    /**
+     * Hashmap of fromEmpty rules (functions)
+     */
     public final static HashMap<String, Function<Pair<Integer, Integer>, Integer>> fromEmpty = new HashMap<>();
+    
+    /**
+     * Hashmap of fromEmpty rules (functions)
+     */
     public final static HashMap<String, Function<Triple<Integer, Integer, Integer>, Integer>> fromOccupied = new
             HashMap<>();
 
     private static Rulesets singleton = new Rulesets();
 
+    /**
+     * Fills hashmaps with classic and diff(different? TODO) ruleset
+     */
     private Rulesets() {
         fromEmpty.put("classic", Ruleset1::fromEmpty);
         fromOccupied.put("classic", Ruleset1::fromOccupied);
@@ -60,6 +97,9 @@ public class Rulesets {
         fromOccupied.put("diff", DiffRuleset::fromOccupied);
     }
 
+    /**
+     * @return the only instance of Rulesets
+     */
     public static Rulesets getInstance() {
         return singleton;
     }
